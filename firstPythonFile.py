a34d1e3a058a26125a0a9e93a11b9d19a17d3c3a
German Hoyos <hoyos.a.german@gmail.com>
2023-04-11 17:57:47 -0400

    2nd attempt at bouncng ball

--- firstPythonFile.py
+++ firstPythonFile.py
@@ -22,14 +22,9 @@ prev_time = time.time() # critical for init of timing mehcanics line 40
 VELOCITY_1 =    100     # traverses 800px in about 8.5 seconds # is striclty a speed
 VELOCITY_2 =    10      # first delta time iteration
 GROUND =        (600)   # bottom of the screen
-GRAVITY =       100     # traverses 800px in about 8.5 seconds
+GRAVITY =       0.5     # traverses 800px in about 8.5 seconds
+BOUNCE_STOP =   0.3       # This prevents infinite tiny bounces
 DELTA_TIME =    float
-
-#"ACCELERATION" is a special case variable that involves some math"
-ACCELERATION = 1         
-    # to be used with gravity # is a measure of the speed and direction of motion
-    # eg: ACCELERATION = DELTATIME * VELOCITY / TIME
-    # TIME could = the milliseconds the object is not at rest on ground level
 
 #Other Global variables
 done = False                        # is game seq complete
@@ -50,19 +45,18 @@ def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     DISPLAYSURF.blit(img, (x, y)) #blit text image to said display surface
 
-#Experimental unit circle functions
+#Experimental unit circle functions using "arc" and "circle"
 def draw_unit_circle_1():
     pygame.draw.arc(DISPLAYSURF, RED, (700,40,20,20), 0, 2 * (math.pi)) #Rect(left, top, width, height)
-
 def draw_unit_circle_2():
     pygame.draw.circle(DISPLAYSURF, GREEN, (750,50), 10, width=1) # a radius of 1 is literaly 1 pixel # width = hollow
 
-####################Asteroid Circle Class####################
+####################Asteroid Player1 Circle Class####################
 class AsteroidPlayer(): # Use this circle for asteroids like movement
     def __init__( #think of this as a constructor u r used to in other languages
             self,
-            thisX = 720,
-            thisY = 90,
+            thisX = 700,
+            thisY = 60,
             thisAngleInteger = 1,
             thisAngle = 0 / 180 * math.pi,
             thisDT = DELTA_TIME,
@@ -93,7 +87,7 @@ class AsteroidPlayer(): # Use this circle for asteroids like movement
         self.draw_unit_circle = pygame.draw.circle(DISPLAYSURF, RED, (self.thisX, self.thisY), 10, width = 1)
         self.movement()
 
-####################Stationary Unit Circle####################
+####################Stationary Unit Circle Shows cos sin tan####################
 class UnitCircle():
     def __init__(self, myX = 570, myY = 50, angle = 0 / 180 * math.pi, rotation = 0):
         self.myX = myX
@@ -115,15 +109,50 @@ class UnitCircle():
         self.draw_cos = pygame.draw.line(DISPLAYSURF, WHITE, (self.myX, self.myY),#<-------------------------------COS
             (self.myX + 49 * math.cos(self.myAngle), self.myY))         
 
-
-
 ####################Bouncing Ball Class####################
+####################Following Tutorial####################
+####################https://www.youtube.com/watch?v=5j0uU3aJxJM&t=1204s####################
+####################Current Time Stamp 27:04####################
+class Ball:
+    def __init__(self, myX, myY, radius, color, mass, retention, ySpeed, xSpeed, id):
+        self.myX = myX
+        self.myY = myY
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        self.retention = retention
+        self.ySpeed = ySpeed
+        self.xSpeed = xSpeed
+        self.id = id
+        self.circle = ''
+    
+    def draw(self):
+        self.circle = pygame.draw.circle(DISPLAYSURF, RED, (self.myX, self.myY), self.radius)
+        self.ySpeed = self.check_gravity()
+        self.update_pos()
 
-
+    def check_gravity(self):
+        if self.myY < 600 - self.radius:
+            self.ySpeed += GRAVITY # gradualy increases over time
+        else:
+            if self.ySpeed > BOUNCE_STOP: # if my y speed is great enough where I want to contiue bouncing
+                self.ySpeed = self.ySpeed * -1 * self.retention # flip direction and lose energy # UPWARD ^
+            else:
+                if abs(self.ySpeed) <= BOUNCE_STOP:
+                    self.ySpeed = 0 # this prevents this.ySpeed being less than bounce stop so much its negative
+                    # which would allow the previous if statement to be true
+        return self.ySpeed
+    
+    def update_pos(self):
+        self.myY += self.ySpeed
+        self.myX += self.xSpeed
 
 ####################Instantiate Objects Outside Of Game loop####################
 C1 = AsteroidPlayer()
 C2 = UnitCircle()
+
+ball1 = Ball(50, 50, 30, 'RED', 100, .9, 0, 0, 1)
+
 ####################Game Loop####################
 while not done:  
     for event in pygame.event.get():  
@@ -149,7 +178,7 @@ while not done:
     ####################Clear Screen####################
     DISPLAYSURF.fill(BLACK)
     
-    ####################Timing Display####################
+    ####################Timing Display Logic####################
     draw_text("MINUTES:",text_font, GREEN, 10, 10)
     draw_text("SECONDS:",text_font, GREEN, 10, 25)
     draw_text("FPS: ",text_font, GREEN, 10, 40)
@@ -178,6 +207,11 @@ while not done:
     #Draw dynamic unit circles
     C1.draw()
     C2.draw()
+
+    #Draw ball experiment
+    ball1.draw()
+    # ball1.update_pos()
+    # ball1.ySpeed = ball1.check_gravity();
 
     ####################REFRESH LOGIC####################
     #redraws entire DISPLAYSURF, but does not clear DISPLAYSURF
